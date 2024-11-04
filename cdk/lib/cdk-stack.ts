b2f7b8bb36e7e8b48719ef5b82b25aeff8a44362
hitohata <hirohatatro@gmail.com>
2024-11-04 12:31:12 -0800

    fix: cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,20 +2,16 @@ import * as path from "node:path";
 import * as cdk from "aws-cdk-lib";
 import { RustFunction } from "cargo-lambda-cdk";
 import type { Construct } from "constructs";
-import * as apiGw from "aws-cdk-lib/aws-apigateway";
 
 export class CdkStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		const rustLambdaFunction = new RustFunction(this, "RustFunction", {
-			functionName: "RustFunctionForAxumTest",
+		new RustFunction(this, "RustFunction", {
+			functionName: "my-notification-function",
 			manifestPath: path.join(__dirname, "../../lambda"),
 			runtime: "provided.al2023",
 		});
 
-		new apiGw.LambdaRestApi(this, "MyAPI", {
-			handler: rustLambdaFunction,
-		});
 	}
 }
